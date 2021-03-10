f5023b9be11ed51d3308b4eb289ba32aa6e0d541
roynaldi19 <roynaldi19@gmail.com>
2021-03-10 16:35:18 +0700

    xml ok v1

--- app/src/main/java/com/roynaldi19/rzikir/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/rzikir/MainActivity.kt
@@ -2,10 +2,17 @@ package com.roynaldi19.rzikir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+
+    TextCount
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
