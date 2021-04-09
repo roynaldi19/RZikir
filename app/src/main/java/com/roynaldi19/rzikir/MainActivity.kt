3568e95d558471f07c2420df3a0f9bc9757352b2
roynaldi19 <roynaldi19@gmail.com>
2021-04-09 18:48:52 +0700

    comit 2.3

--- app/src/main/java/com/roynaldi19/rzikir/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/rzikir/MainActivity.kt
@@ -2,6 +2,9 @@ package com.roynaldi19.rzikir
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.roynaldi19.rzikir.databinding.ActivityMainBinding
 
@@ -14,10 +17,13 @@ class MainActivity : AppCompatActivity() {
     var count = 0
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
 
         binding.btnIstigfar.setOnClickListener {
@@ -25,9 +31,33 @@ class MainActivity : AppCompatActivity() {
             mediaPlayer?.start();
             count++
 
+            binding.tvCount.setText("makan lah")
+
+
         }
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_reset -> {
+                mediaPlayer?.stop()
+                count = 0;
+
+            }
+        }
+    }
+
 
 
 }
